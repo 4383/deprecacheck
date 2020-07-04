a36051f44e5d543f5000122f848648e3c992dc10
Hervé Beraud <hberaud@redhat.com>
2020-07-04 13:42:23 +0200

    still in development message

--- wardoff/__main__.py
+++ wardoff/__main__.py
@@ -3,6 +3,7 @@ from wardoff import argparser
 
 def main():
     args = argparser().parse_args()
+    print("Still in development nothing to return for now.")
     analyzer = args.project
     _ = args.output
     analyzer.analyze()
